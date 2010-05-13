90211ad39ce438c1a88178216fc26b17866d3f99
Volker Mische <volker.mische@gmail.com>
2010-05-14 00:33:41 +0200

    Added tests.

--- wkt.erl
+++ wkt.erl
@@ -1,21 +1,11 @@
 % The code is heavily based on MochiWeb's JSON decoder.
 
-% Examples from Wikipedia article
-% http://en.wikipedia.org/wiki/Simple_Features (2010-05-07)
-% POINT (10 10)
-% LINESTRING( 10 10, 20 20, 30 40)
-% POLYGON ((10 10, 10 20, 20 20, 20 15, 10 10))
-% MULTIPOINT(10 10, 20 20)
-% MULTIPOLYGON(((10 10, 10 20, 20 20, 20 15, 10 10)),((60 60, 70 70, 80 60, 60 60)))
-% GEOMETRYCOLLECTION(POINT (10 10), POINT(30 30), LINESTRING(15 15, 20 20))
-
-% For OGC specification:
-%MultiLineString((10 10, 20 20), (15 15, 30 15))
-
 -module(wkt).
 -include_lib("eunit/include/eunit.hrl").
 
 -export([parse/1]).
+
+% TODO: support EMPTY instead of ().
 
 parse(Wkt) ->
     case parse_char(Wkt) of
@@ -68,7 +58,10 @@ parse_list_inner(Wkt, Acc) ->
         Acc2 = tuple_them(Acc),
         {{parsed_list, lists:reverse(Acc2)}, Wkt2};
     {space, Wkt2} ->
-        parse_list(Wkt2, Acc);
+        %parse_list(Wkt2, Acc);
+        parse_list_inner(Wkt2, Acc);
+    {{parsed, Parsed}, Wkt2} ->
+        parse_list_inner(Wkt2, [Parsed|Acc]);
     {comma, Wkt2} ->
         Acc2 = tuple_them(Acc),
         parse_list(Wkt2, Acc2)
@@ -107,11 +100,10 @@ parse_number([H|T], float, Acc) when H == $E orelse H == $- ->
     parse_number(T, float, [H|Acc]);
 parse_number([H|T], Type, Acc) when (H >= $0) and (H =< $9) ->
     parse_number(T, Type, [H|Acc]);
-parse_number([H|T], Type, Acc) when H == $. ->
+parse_number([H|T], _Type, Acc) when H == $. ->
     parse_number(T, float, [H|Acc]);
 parse_number(Wkt, Type, Acc) ->
     {lists:reverse(Acc), Type, Wkt}.
-
 
 
 parse_geometry(Wkt) ->
@@ -139,87 +131,123 @@ parse_atom([H|T], Acc) when ((H >= $a) and (H =< $z)) orelse
     parse_atom(T, [H|Acc]);
 parse_atom(Wkt, Acc) ->
     Stripped = string:strip(Acc, left),
-    {lists:reverse(Stripped), Wkt}.
+    {lists:reverse(string:to_lower(Stripped)), Wkt}.
 
 
-parse_test() ->
-    %Result = parse("(10 11 13, 21 23 46, 47 58 69)"),
-    %Result = parse("((10 11, 13 15), (21 23, 46 47), (47 58, 69 85))"),
-    %Result = parse("((10 11))"),
-    Result = parse("(((10 11)))"),
-    io:format("Results: ~p~n", [Result]).
+
+parse_whitespace_single_test() ->
+    Point = {point, [{12, 13}]},
+    ?assertEqual(Point, parse("POINT(12 13)")),
+    ?assertEqual(Point, parse("POINT (12 13)")),
+    ?assertEqual(Point, parse("POINT  (12 13)")),
+    ?assertEqual(Point, parse("POINT( 12 13)")),
+    ?assertEqual(Point, parse("POINT(  12 13)")),
+    ?assertEqual(Point, parse("POINT(12  13)")),
+    ?assertEqual(Point, parse("POINT(12 13 )")),
+    ?assertEqual(Point, parse("POINT(12 13  )")),
+    ?assertEqual(Point, parse("POINT(12  13  )")).
+
+parse_whitespace_multi_test() ->
+    LS = {linestring, [{12, 13}, {14, 15}, {16, 17}]},
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13, 14 15,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,  14 15,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15, 16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15,  16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13, 14 15,  16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13 ,14 15,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13  ,14 15,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13, 14 15 ,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,  14 15  ,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,  14 15  , 16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,  14 15  ,  16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13 ,  14 15  ,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13  ,  14 15  ,16 17)")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15,16 17 )")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15,16 17  )")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15, 16 17 )")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15, 16 17  )")),
+    ?assertEqual(LS, parse("LINESTRING(12 13,14 15,  16 17  )")).
+
+parse_whitespace_geometrycollection_test() ->
+    GC = {geometrycollection, [{point, [{12, 13}]}]},
+    GC2 = {geometrycollection, [{point, [{12, 13}]}, {point, [{14, 15}]}]},
+    ?assertEqual(GC, parse("GEOMETRYCOLLECTION( POINT(12 13))")),
+    ?assertEqual(GC, parse("GEOMETRYCOLLECTION(  POINT(12 13))")),
+    ?assertEqual(GC, parse("GEOMETRYCOLLECTION(POINT(12 13) )")),
+    ?assertEqual(GC, parse("GEOMETRYCOLLECTION(POINT(12 13)  )")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION( POINT(12 13),POINT(14 15))")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(  POINT(12 13),POINT(14 15))")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13),POINT(14 15))")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13), POINT(14 15))")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13),  POINT(14 15))")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13),POINT(14 15) )")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13),POINT(14 15)  )")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13) ,POINT(14 15))")),
+    ?assertEqual(GC2, parse("GEOMETRYCOLLECTION(POINT(12 13)  ,POINT(14 15))")).
 
 
-%parse_test() ->
-%    Result1 = parse("point(10 10)"),
-%    Result2 = parse("LINESTRING(10 10, 20 20, 30 40)"),
-%    Result3 = parse("POLYGON ((10 11, 10 21, 20 21, 20 15, 10 11))"),
-%    Result4 = parse("POLYGON ((0.0 0.0, 0.0 1.0, 1.0 1.0, 1.0 0.0, 0.0 0.0), (0.2 0.2, 0.2 0.8, 0.8 0.8, 0.8 0.2, 0.2 0.2))"),
-%    Result5 = parse("MULTIPOLYGON(((10 10, 10 20, 20 20, 20 15, 10 10)),((60 60, 70 70, 80 60, 60 60)))"),
-%    Result6 = parse("POLYGON ((0.0 0.0, 0.0 1.0), (0.2 0.2, 0.2 0.8), (0.5 0.5, 0.5 0.6))"),
-%    io:format("Result1: ~p~n", [Result1]),
-%    io:format("Result2: ~p~n", [Result2]),
-%    io:format("Result3: ~p~n", [Result3]),
-%    io:format("Result4: ~p~n", [Result4]),
-%    io:format("Result5: ~p~n", [Result5]),
-%    io:format("Result6: ~p~n", [Result6]).
-%    %?assertEqual(something, Result).
+parse_geom_point_test() ->
+    ?assertEqual({point, [{12, 13}]}, parse("POINT (12 13)")).
 
-%scan_test() ->
-%    Result = scan("point(10 10)"),
-    %Result = scan("point(10.5 -0.11)"),
-    %Result = scan("LINESTRING(10 10, 20 20, 30 40)"),
-    %Result = scan("POLYGON ((10 11, 10 21, 20 21, 20 15, 10 11))"),
-    %p = Polygon(((0, 0), (0, 1), (1, 1), (1, 0)), [((0.2, 0.2), (0.2, 0.8), (0.8, 0.8), (0.8, 0.2))])
-    %Result = scan("POLYGON ((0.0 0.0, 0.0 1.0, 1.0 1.0, 1.0 0.0, 0.0 0.0), (0.2 0.2, 0.2 0.8, 0.8 0.8, 0.8 0.2, 0.2 0.2))"),
-    %Result = scan("POLYGON ((0.0 0.0, 0.0 1.0) , )"),
-    %Result = scan("POLYGON ((0.0 0.0, 0.0 1.0), (0.2 0.2, 0.2 0.8), (0.5 0.5, 0.5 0.6))"),
-    %p = Polygon(((0, 0), (0, 1), (1, 1), (1, 0)), [((0.2, 0.2), (0.2, 0.3), (0.3, 0.3), (0.3, 0.2)), ((0.5, 0.5), (0.5, 0.6), (0.6, 0.6), (0.6, 0.5))])
-    %Result = scan("POLYGON ((0.0 0.0, 0.0 1.0, 1.0 1.0, 1.0 0.0, 0.0 0.0), (0.2 0.2, 0.2 0.3, 0.3 0.3, 0.3 0.2, 0.2 0.2), (0.5 0.5, 0.5 0.6, 0.6 0.6, 0.6 0.5, 0.5 0.5))"),
-    %Result = scan("MULTIPOINT(10 10, 20 20)"),
-    %Result = scan("MultiLineString((10 10, 20 20), (15 15, 30 15))"),
-    %Result = scan("MULTIPOLYGON(((10 10, 10 20, 20 20, 20 15, 10 10)),((60 60, 70 70, 80 60, 60 60)))"),
-%    Result = scan("GEOMETRYCOLLECTION(POINT (10 10), POINT(30 30), LINESTRING(15 15, 20 20))"),
-    %Result = scan("G(10)"),
-    %Result = scan("Gewrw(Pewr(10 10))"),
-%    io:format("Results: ~p~n", [Result]).
-    %?assertEqual(something, scan("point(10 10)")).
+parse_geom_linestring_test() ->
+    ?assertEqual({linestring, [{12, 13}, {14, 15}]},
+                 parse("LINESTRING (12 13, 14 15)")).
 
-%scan_ws_test() ->
-%    ?assertEqual({ok,[{text,{1,2},"p"}]}, scan(" p")),
-%    %?assertEqual({ok,[{text,{1,2},"p"}]}, scan("p")),
-%    Expected = {ok,[{text,{1,1},"point"},
-%     {open_coords_list,{1,7},["["]},
-%     {open_coords,{1,8},["["]},
-%     {number_literal,{1,8},"10"},
-%     {number_literal,{1,11},"11"},
-%     {close_coords,{1,13},["]"]},
-%     {close_coords_list,{1,13},["]"]}]},
-%    ?assertEqual(Expected, scan("point (10 11)")),
-%    Expected2 = {ok,[{text,{1,1},"LINESTRING"},
-%     {open_coords_list,{1,11},["["]},
-%     {open_coords,{1,12},["["]},
-%     {number_literal,{1,12},"10"},
-%     {number_literal,{1,15},"10"},
-%     {close_coords,{1,17},["]"]},
-%     {open_coords,{1,19},["["]},
-%     {number_literal,{1,19},"20"},
-%     {number_literal,{1,22},"20"},
-%     {close_coords,{1,24},["]"]},
-%     {close_coords_list,{1,24},["]"]}]},
-%    io:format("~p", [scan("LINESTRING(10 10, 20 20)")]).
-%    %?assertEqual(Expected2, scan("LINESTRING(10 10, 20 20)")),
-%    %?assertEqual(Expected2, scan("LINESTRING(10 10,20 20)")).
+parse_geom_polygon_test() ->
+    ?assertEqual({polygon, [[{12, 13}, {24, 25}, {36, 17}, {12, 13}]]},
+                 parse("POLYGON ((12 13, 24 25, 36 17, 12 13))")),
+    ?assertEqual({polygon, [[{102, 103}, {204, 205}, {306, 107}, {102, 103}],
+                            [{12, 13}, {24, 25}, {36, 17}, {12, 13}]]},
+                 parse("POLYGON ((102 103, 204 205, 306 107, 102 103),"
+                       "(12 13, 24 25, 36 17, 12 13))")),
+    ?assertEqual({polygon, [[{102, 103}, {204, 205}, {306, 107}, {102, 103}],
+                            [{12, 13}, {24, 25}, {36, 17}, {12, 13}],
+                            [{62, 63}, {74, 75}, {86, 67}, {62, 63}]]},
+                 parse("POLYGON ((102 103, 204 205, 306 107, 102 103),"
+                       "(12 13, 24 25, 36 17, 12 13),"
+                       "(62 63, 74 75, 86 67, 62 63))")).
 
-%scan_minus_test() ->
-%    Result = scan("point(-10 11)"),
-%    ?assertEqual(something, Result).
+parse_geom_multipoint_test() ->
+    ?assertEqual({multipoint, [{12, 13}, {14, 15}]},
+                 parse("MULTIPOINT (12 13, 14 15)")).
 
+parse_geom_multilinestring_test() ->
+    ?assertEqual({multilinestring, [[{12, 13}, {14, 15}],
+                                    [{16, 17}, {18, 19}]]},
+                 parse("MULTILINESTRING ((12 13, 14 15), (16 17, 18 19))")).
 
-%scan_parenthesis_test() ->
-%    Result = scan("point(-10)"),
-%    ?assertEqual(something, Result),
-%    Result2 = scan("point(-10 11)"),
-%    ?assertEqual(something, Result2),
-%    Result3 = scan("point((-10 11)"),
-%    ?assertEqual(something, Result3).
+parse_geom_multipolygon_test() ->
+    ?assertEqual({multipolygon, [[[{12, 13}, {24, 25}, {36, 17}, {12, 13}]],
+                                 [[{2012, 2013}, {2024, 2025},
+                                   {2036, 2017}, {2012, 2013}]]]},
+                 parse("MULTIPOLYGON (((12 13, 24 25, 36 17, 12 13)),"
+                       "((2012 2013, 2024 2025, 2036 2017, 2012 2013)))")),
+    ?assertEqual({multipolygon, [[[{102, 103}, {204, 205}, {306, 107}, {102, 103}],
+                                  [{12, 13}, {24, 25}, {36, 17}, {12, 13}]],
+                                 [[{2102, 2103}, {2204, 2205},
+                                   {2306, 2107}, {2102, 2103}],
+                                  [{2012, 2013}, {2024, 2025},
+                                   {2036, 2017}, {2012, 2013}]]]},
+                 parse("MULTIPOLYGON (((102 103, 204 205, 306 107, 102 103),"
+                       "(12 13, 24 25, 36 17, 12 13)),"
+                       "((2102 2103, 2204 2205, 2306 2107, 2102 2103),"
+                       "(2012 2013, 2024 2025, 2036 2017, 2012 2013)))")).
+
+parse_geom_geometrycollection_test() ->
+    ?assertEqual({geometrycollection,[{point, [{12, 13}]}]},
+                 parse("GEOMETRYCOLLECTION(POINT (12 13))")),
+    ?assertEqual({geometrycollection,[{point, [{12, 13}]},
+                                      {multipoint, [{12, 13}, {14, 15}]}]},
+                 parse("GEOMETRYCOLLECTION(POINT (12 13),"
+                       "MULTIPOINT (12 13, 14 15))")),
+    ?assertEqual({geometrycollection,
+                  [{point, [{12, 13}]}, {point, [{14, 15}]},
+                   {polygon, [[{102, 103}, {204, 205}, {306, 107}, {102, 103}],
+                              [{12, 13}, {24, 25}, {36, 17}, {12, 13}],
+                              [{62, 63}, {74, 75}, {86, 67}, {62, 63}]]}]},
+                 parse("GEOMETRYCOLLECTION("
+                       "POINT (12 13),POINT (14 15),"
+                       "POLYGON ((102 103, 204 205, 306 107, 102 103),"
+                       "(12 13, 24 25, 36 17, 12 13),"
+                       "(62 63, 74 75, 86 67, 62 63)))")).
